d6b1d51d166c01c0453b52b5df223d835f909458
Dana Yakoobinsky <danayakoobinsky@danas-air.lan>
2024-04-04 21:40:52 -0700

    Fix paths to assets in config file

--- .eleventy.js
+++ .eleventy.js
@@ -2,9 +2,9 @@ const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 const pluginRss = require("@11ty/eleventy-plugin-rss");
 
 module.exports = function(eleventyConfig) {
-    eleventyConfig.addPassthroughCopy("assets/styles/bundle.css");
-    eleventyConfig.addPassthroughCopy("assets/images");
-    eleventyConfig.addWatchTarget("assets/styles/bundle.css");
+    eleventyConfig.addPassthroughCopy("src/assets/styles/bundle.css");
+    eleventyConfig.addPassthroughCopy("src/assets/images");
+    eleventyConfig.addWatchTarget("src/assets/styles/bundle.css");
     eleventyConfig.addPlugin(syntaxHighlight);
     eleventyConfig.addPlugin(pluginRss);
     eleventyConfig.addLiquidFilter("dateToRfc822", pluginRss.dateToRfc822);
