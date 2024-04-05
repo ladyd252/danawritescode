192615cd3083be57e6d659a996b1757adfa11374
Dana Yakoobinsky <danayakoobinsky@danas-air.lan>
2024-04-04 21:28:08 -0700

    Move content to src directory, add rss feed

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,17 @@
 const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight"); 
+const pluginRss = require("@11ty/eleventy-plugin-rss");
 
 module.exports = function(eleventyConfig) {
-    eleventyConfig.addPassthroughCopy("bundle.css");
+    eleventyConfig.addPassthroughCopy("assets/styles/bundle.css");
     eleventyConfig.addPassthroughCopy("assets/images");
-    eleventyConfig.addWatchTarget("bundle.css");
+    eleventyConfig.addWatchTarget("assets/styles/bundle.css");
     eleventyConfig.addPlugin(syntaxHighlight);
+    eleventyConfig.addPlugin(pluginRss);
+    eleventyConfig.addLiquidFilter("dateToRfc822", pluginRss.dateToRfc822);
+
+    return {
+        dir: {
+          input: "src"
+        }
+      }
 };
